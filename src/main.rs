43235750592da8f2bad95719f9c8dcd2d9dc0ff7
Jackson <jacksonludwig0@gmail.com>
2020-07-29 11:33:49 -0400

    test lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 use smite_api_library::json;
 
-fn main() {}
+fn main() {
+    let session = json::make_session().unwrap();
+    let req = json::create_link(
+        "getplayer",
+        &session.session_id,
+        &json::get_formatted_time(),
+    );
+    println!("{}", req);
+    println!("Timestamp: {}", session.timestamp);
+}
