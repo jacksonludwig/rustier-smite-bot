7b20182eb43f31711ecd218e04febfe7bc7c6d79
Jackson <jacksonludwig0@gmail.com>
2020-07-29 21:20:15 -0400

    add todo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use smite_api_library::json;
 
+// TODO:
+// Keep track of the time of each new query attempt,
+// if it has been more than 15 min since last request,
+// then get a new session id.
+
 fn main() {
     let session = json::make_session().unwrap();
     let req = json::create_link(
