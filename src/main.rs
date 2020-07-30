a5674379226f0ac9e13033b726bdf466c96b2af9
Jackson <jacksonludwig0@gmail.com>
2020-07-30 14:52:16 -0400

    tract time

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,30 @@ use smite_api_library::queries::QueryBuilder;
 // then get a new session id.
 
 fn main() {
-    let session = json::make_session().unwrap_or_else(|err| {
-        panic!("There was an error creating the session: {:?}", err);
-    });
+    test_loop();
+}
 
-    let builder = QueryBuilder::new(session.session_id, session.timestamp);
-    let json_data = builder.get_player("SwiggedySwoody").unwrap();
-    println!("{}", json_data);
+fn test_loop() {
+    let mut session = json::make_session().unwrap();
+    let mut builder = QueryBuilder::new(session.session_id);
+    loop {
+        let time_dif = json::get_formatted_time().parse::<usize>().unwrap()
+            - session.timestamp.parse::<usize>().unwrap();
+        println!("Seconds from last query: {}", time_dif);
+        if time_dif > 900 {
+            session = json::make_session().unwrap();
+            builder = QueryBuilder::new(session.session_id);
+            println!("A new session was constructed");
+        }
+
+        println!("Type something to make the query");
+        let mut answer = String::new();
+        std::io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+
+        let json_data = builder.get_player("SwiggedySwoody").unwrap();
+
+        println!("{}", json_data);
+    }
 }
