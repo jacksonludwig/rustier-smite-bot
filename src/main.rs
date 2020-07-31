7191963e5b3d06f4ee2868234bd6624420a181f6
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-07-31 14:03:03 -0400

    download a read back json

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
 use smite_api_library::json;
 use smite_api_library::queries::QueryBuilder;
 
+mod transformer;
+
 fn main() {
-    // test_loop();
     let session = json::make_session().unwrap();
     let builder = QueryBuilder::new(session.session_id);
-    let data = builder.get_gods();
-}
-
-fn test_loop() {
-    let mut session = json::make_session().unwrap();
-    let mut builder = QueryBuilder::new(session.session_id);
-    loop {
-        let time_dif = json::get_formatted_time().parse::<usize>().unwrap()
-            - session.timestamp.parse::<usize>().unwrap();
-        println!("Seconds from last query: {}", time_dif);
-        if time_dif > 900 {
-            session = json::make_session().unwrap();
-            builder = QueryBuilder::new(session.session_id);
-            println!("A new session was constructed");
-        }
-
-        println!("Type something to make the query");
-        let mut answer = String::new();
-        std::io::stdin()
-            .read_line(&mut answer)
-            .expect("Failed to read line");
-
-        let json_data = builder.get_player(&answer).unwrap();
-
-        println!("{}", json_data);
+    let god_list = transformer::make_god_list();
+    for god in god_list {
+        println!("Name: {}, id: {}", god.name, god.id);
     }
 }
---
+++ src/transformer.rs
@@ -0,0 +1,42 @@
+use serde_json::{Result, Value};
+use smite_api_library::json;
+use smite_api_library::queries::QueryBuilder;
+
+const GOD_JSON_DIR: &str = "resources/gods.json";
+
+/// Download the god data.
+pub fn store_god_json(builder: &QueryBuilder) -> Result<()> {
+    // TODO: Handle this
+    let data = builder.get_gods().unwrap();
+
+    let _ = json::write_string_to_file(GOD_JSON_DIR, data).unwrap();
+
+    Ok(())
+}
+
+pub struct God {
+    pub name: String,
+    pub id: String,
+}
+
+impl God {
+    fn new(name: String, id: String) -> Self {
+        God { name, id }
+    }
+}
+
+/// Read the god data and make it into a Vec of Gods, which only store the name
+/// and id.
+pub fn make_god_list() -> Vec<God> {
+    let data = json::read_file_to_string(GOD_JSON_DIR).unwrap();
+    let values: Vec<Value> = serde_json::from_str(&data).unwrap();
+    let mut god_tuples: Vec<God> = vec![];
+    for god in values {
+        let tuple = God::new(
+            god["Name"].as_str().unwrap().to_string(),
+            god["id"].to_string(),
+        );
+        god_tuples.push(tuple);
+    }
+    god_tuples
+}
