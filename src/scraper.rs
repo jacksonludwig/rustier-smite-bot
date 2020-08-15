04939436650e8625290ecf556068d2dcb0b3043e
jackson <jackson@pop-os.localdomain>
2020-08-14 23:16:40 -0400

    add tokio and fantoccini deps

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,17 +1,15 @@
-use reqwest;
 use soup::prelude::*;
 use soup::Soup;
 
 const BASE_LINK: &str = "https://smitesource.com";
 
 // TODO: This needs to use a headless webdriver to load html from page javascript...
-pub fn fetch_html(link: &str) -> Result<String, reqwest::Error> {
-    let result = reqwest::blocking::get(link)?.text()?;
-    Ok(result)
+pub fn fetch_html(link: &str) -> String {
+    String::from("placeholder")
 }
 
 pub fn get_god_build_list(link: &str) -> Result<Vec<String>, reqwest::Error> {
-    let page = fetch_html(link)?;
+    let page = fetch_html(link);
     let soup = Soup::new(&page);
     let build_cards = soup
         .tag("a")
